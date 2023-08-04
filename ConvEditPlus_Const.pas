856999b980f50f28fa51e71e56e70b0c03decf29
LoadLine Calibration <Veronika-and@yandex.ru>
2023-08-04 16:25:05 +0900

    Moving constants into separate unit

---
+++ ConvEditPlus_Const.pas
@@ -0,0 +1,11 @@
+unit ConvEditPlus_Const;
+
+interface
+
+const CEP_EVENT_LIST_FONT_NAME = 'Consolas';
+const CEP_EVENT_LIST_FONT_SIZE = 10;
+
+
+implementation
+
+end.
---
+++ ConvEditor.dpr
@@ -0,0 +1,36 @@
+program ConvEditor;
+
+uses
+  Vcl.Forms,
+  MainWindow in 'MainWindow.pas' {frmMain},
+  AddInsertEvent in 'AddInsertEvent.pas' {formEventInsAdd},
+  Table in 'Table.pas' {frmTableEdit},
+  ConvoProperties in 'ConvoProperties.pas' {frmConvoProperties},
+  EditValueDialog in 'EditValueDialog.pas' {frmEditValue},
+  ConversationFilePropertiesDialog in 'ConversationFilePropertiesDialog.pas' {frmConvoFileProperties},
+  EditChoice in 'EditChoice.pas' {frmEditChoice},
+  Vcl.Themes,
+  Vcl.Styles,
+  HelpWindow in 'HelpWindow.pas' {frmHelp},
+  frmFlagList1 in 'frmFlagList1.pas' {frmFlagList},
+  frmSettings1 in 'frmSettings1.pas' {frmSettings},
+  ConvEditPlus_Const in 'ConvEditPlus_Const.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TformEventInsAdd, formEventInsAdd);
+  Application.CreateForm(TfrmTableEdit, frmTableEdit);
+  Application.CreateForm(TfrmConvoProperties, frmConvoProperties);
+  Application.CreateForm(TfrmEditValue, frmEditValue);
+  Application.CreateForm(TfrmConvoFileProperties, frmConvoFileProperties);
+  Application.CreateForm(TfrmEditChoice, frmEditChoice);
+  Application.CreateForm(TfrmHelp, frmHelp);
+  Application.CreateForm(TfrmFlagList, frmFlagList);
+  Application.CreateForm(TfrmSettings, frmSettings);
+  Application.HintHidePause := 10000;
+  Application.Run;
+end.
