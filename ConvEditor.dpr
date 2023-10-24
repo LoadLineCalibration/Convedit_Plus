d7f734b25fb387f8e5ea0b797eebd999f940d5be
LoadLine Calibration <Veronika-and@yandex.ru>
2023-10-24 14:52:50 +0900

    Project source files

--- ConvEditor.dpr
+++ ConvEditor.dpr
@@ -1,23 +1,32 @@
+{
+    ConvEdit Plus project.
+    Created in Delphi 11
+
+
+    Author: Veronika Afanasyeva (Andrievskaya) aka LoadLine Calibration
+
+    Open-source. Feel free to modify...
+}
+
 program ConvEditor;
 
 uses
   Vcl.Forms,
   MainWindow in 'MainWindow.pas' {frmMain},
-  AddInsertEvent in 'AddInsertEvent.pas' {formEventInsAdd},
   Table in 'Table.pas' {frmTableEdit},
   ConvoProperties in 'ConvoProperties.pas' {frmConvoProperties},
   EditValueDialog in 'EditValueDialog.pas' {frmEditValue},
-  ConversationFilePropertiesDialog in 'ConversationFilePropertiesDialog.pas' {frmConvoFileProperties},
+  ConFileProperties in 'ConFileProperties.pas' {frmConvoFileProperties},
   EditChoice in 'EditChoice.pas' {frmEditChoice},
-  Vcl.Themes,
-  Vcl.Styles,
   HelpWindow in 'HelpWindow.pas' {frmHelp},
   frmFlagList1 in 'frmFlagList1.pas' {frmFlagList},
   frmSettings1 in 'frmSettings1.pas' {frmSettings},
   ConvEditPlus_Const in 'ConvEditPlus_Const.pas',
   ConvEditPlus_Util in 'ConvEditPlus_Util.pas',
   frmFind1 in 'frmFind1.pas' {frmFind},
-  AboutBox1 in 'AboutBox1.pas' {frmAbout};
+  AboutBox1 in 'AboutBox1.pas' {frmAbout},
+  AddInsertEvent in 'AddInsertEvent.pas' {frmEventInsAdd},
+  Conversation.Classes in 'Conversation.Classes.pas';
 
 {$R *.res}
 
@@ -25,7 +34,7 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrmMain, frmMain);
-  Application.CreateForm(TformEventInsAdd, formEventInsAdd);
+  Application.CreateForm(TfrmEventInsAdd, frmEventInsAdd);
   Application.CreateForm(TfrmTableEdit, frmTableEdit);
   Application.CreateForm(TfrmConvoProperties, frmConvoProperties);
   Application.CreateForm(TfrmEditValue, frmEditValue);
@@ -36,6 +45,7 @@ begin
   Application.CreateForm(TfrmSettings, frmSettings);
   Application.CreateForm(TfrmFind, frmFind);
   Application.CreateForm(TfrmAbout, frmAbout);
+  Application.CreateForm(TfrmEventInsAdd, frmEventInsAdd);
   Application.HintHidePause := 10000;
   Application.Run;
 end.
